9c956e9abf70be2da270e9ee9badca27cc58a7ae
Souvik Sarkar <89373683+Souvik2024@users.noreply.github.com>
2023-03-20 19:29:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
-import yfinance as yf
 import streamlit as st
+import pandas as pd
 
-st.write("""
-# Simple Stock Price App
+df = pd.DataFrame(
+    [
+       {"command": "st.selectbox", "rating": 4, "is_widget": True},
+       {"command": "st.balloons", "rating": 5, "is_widget": False},
+       {"command": "st.time_input", "rating": 3, "is_widget": True},
+   ]
+)
+edited_df = st.experimental_data_editor(df, num_rows="dynamic")
 
-Shown are the stock **closing price** and ***volume*** of Google!
-
-""")
-
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
-
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
